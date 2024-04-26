c726fe0e22b236c926eb8f1484c48d3322dae60d
ebidicey <157209503+ebidicey@users.noreply.github.com>
2024-04-26 20:06:56 +0100

    Add files via upload

---
+++ process.form.php
@@ -0,0 +1,23 @@
+<?php
+  $name = $_Post['name'];
+  $status = $_Post['status'];
+  $date = $_Post['date'];
+  $street = $_Post['street'];
+  $town = $_Post['town'];
+  $lga = $_Post['lga'];
+  $email = $_Post['email'];
+  $phone = $_Post['phone'];
+  $gender = $_Post['gender'];
+  $hearing = $_Post['hearing'];
+  $physical = $_Post['physical'];
+  $visual = $_Post['visual'];
+  $autism = $_Post['autism'];
+  $downsyndrome = $_Post['downsyndrome'];
+  $adhd = $_Post['adhd'];
+  $others = $_Post['others'];
+
+  var_dump($name, $status, $date, $street,
+  $town, $lga, $email, $phone, $gender, $hearing,
+  $physical, $visual, $autism, $downsyndrome, $adhd, $others);
+
+?>
